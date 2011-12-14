30da7c4b8f431fc1f586ebc0409b84e926a3fe02
abarth500 <domain_github.com@black.cs.inf.shizuoka.ac.jp>
2011-12-14 00:04:20 -0500

    test

---
+++ photomosaic_knn/src/photomosaic_knn.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : photomosaic_knn.cpp
+// Author      : Shohei Yokoyama
+// Version     :
+// Copyright   : Shizuoka University, Japan
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
